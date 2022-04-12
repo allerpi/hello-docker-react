bf8980b19bec42c2131f79ac461fda9090242700
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2022-04-11 20:39:27 -0500

    add screen recording

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useReactMediaRecorder } from "react-media-recorder";
+
 function App() {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ screen: true, audio: false, blobPropertyBag: {type: "video/mp4"}});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> :D
-        </p>
-      </header>
+    <div>
+      <h1 className='center'>React Screen Recorder</h1>
+      <p id='status' >Status: {status.replace('_', ' ')}</p>
+      <div className='buttons'>
+        <button onClick={startRecording} id='start' className='center'>Start Recording</button>
+        <div id='space'></div>
+        <button onClick={stopRecording} id='stop' className='center'>Stop Recording</button>
+      </div>
+      <RecordedVideo blobUrl={mediaBlobUrl} />
     </div>
   );
+};
+
+function RecordedVideo(props) {
+  if(props.blobUrl) {
+    const downloadName = 'screen-recording-'+ new Date().toUTCString();
+    console.log(props.blobUrl);
+    return (
+      <div>
+        <video src={props.blobUrl} controls width='600px'/>
+        <div id='link'>
+        <a download={downloadName} href={props.blobUrl}>Download</a>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div id='no-video'>
+        No recording to display
+      </div>
+    );
+  }
 }
 
 export default App;
