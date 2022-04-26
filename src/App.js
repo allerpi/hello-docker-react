20f52f3ac409d97f7cfeb08463c6f84eb5b4b36c
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2022-04-25 23:18:47 -0500

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-import { useReactMediaRecorder } from "react-media-recorder";
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  const {
-    status,
-    startRecording,
-    stopRecording,
-    mediaBlobUrl,
-  } = useReactMediaRecorder({ screen: true, audio: false, blobPropertyBag: {type: "video/mp4"}});
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
+function App({ signOut, user }) {
   return (
-    <div>
-      <h1 className='center'>React Screen Recorder</h1>
-      <p id='status' >Status: {status.replace('_', ' ')}</p>
-      <div className='buttons'>
-        <button onClick={startRecording} id='start' className='center'>Start Recording</button>
-        <div id='space'></div>
-        <button onClick={stopRecording} id='stop' className='center'>Stop Recording</button>
-      </div>
-      <RecordedVideo blobUrl={mediaBlobUrl} />
-    </div>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
-};
-
-function RecordedVideo(props) {
-  if(props.blobUrl) {
-    const downloadName = 'screen-recording-'+ new Date().toUTCString();
-    console.log(props.blobUrl);
-    return (
-      <div>
-        <video src={props.blobUrl} controls width='600px'/>
-        <div id='link'>
-        <a download={downloadName} href={props.blobUrl}>Download</a>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div id='no-video'>
-        No recording to display
-      </div>
-    );
-  }
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+
